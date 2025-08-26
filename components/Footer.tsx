bd3b5ea7c76712ad3e81b21b4dc17cdc17fd6593
malpisen <malin.rydefalk@gmail.com>
2025-08-26 09:27:16 +0200

    added items to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,35 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <nav className="bg-neutral-800 breakout flex items-center justify-between">
-            <Link href={"/"}>
-                <Image
-                    src="/Logo.png"
-                    width={50}
-                    height={50}
-                    alt="Logo"
-                />
-                <h1 className="text-center text-3xl text-white">Pokédex</h1>
-            </Link>
-            <p className="text-white">Explore the world of Pokémon</p>
             <ul className="flex gap-4">
-
+                <li>
+                    <Link href={"/"}>
+                        <Image
+                            src="/Logo.png"
+                            width={50}
+                            height={50}
+                            alt="Logo"
+                        />
+                        <h1 className="text-center text-3xl text-white">Pokédex</h1>
+                    </Link>
+                </li>
+                <li>
+                    <p className="text-white">Explore the world of Pokémon</p>
+                </li>
+                <li>
+                    <Image
+                        src="/Facebook.svg"
+                        width={40}
+                        height={40}
+                        alt="Facebook"
+                    />
+                    <Image
+                        src="/Instagram.svg"
+                        width={40}
+                        height={40}
+                        alt="Instagram"
+                    />
+                </li>
             </ul>
         </nav>
     );
