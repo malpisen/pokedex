675cdda787ba6cb1c64e873d502d31a36e973655
malpisen <malin.rydefalk@gmail.com>
2025-09-02 13:49:51 +0200

    added not-found for pokemon

---
+++ app/pokemon/[slug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <section className="flex flex-col items-center justify-center min-h-100 bg-gradient-to-br from-pink-200 to-indigo-200">
+            <h1 className="text-8xl">404</h1>
+            <p className="text-xl">Pokémonen du letade efter finns inte! 😢</p>
+        </section>
+    );
+}
