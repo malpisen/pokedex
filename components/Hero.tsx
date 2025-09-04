98bfbd0891263ef576bf07ad8e7d5437a8fbb8bf
malpisen <malin.rydefalk@gmail.com>
2025-09-04 10:02:05 +0200

    changed text color

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
         <section className="flex flex-col items-center gap-4 p-14 bg-gradient-to-br from-blue-200 to-purple-400">
             <h1 className="text-center mt-14 text-8xl text-transparent bg-gradient-to-r from-purple-800 to-blue-800 [background-clip:text]">Gotta catch 'em all!</h1>
-            <p className="text-center text-white text-xl">Discover, search and explore the amazing world of Pokémon. <br />Find your favourite and learn about their stats.</p>
+            <p className="text-center text-xl">Discover, search and explore the amazing world of Pokémon. <br />Find your favourite and learn about their stats.</p>
             <Link
                 className="btn-primary" href={`/pokemon/${getRandomId()}`}>
                 <Image
